728f5013104ff25075c5ea2e1212246bdcf9de41
m-aksoy-altug <m.aksoy.altug@gmail.com>
2025-02-11 18:42:42 +1100

    initial push

---
+++ src/main/java/org/aws/spring/SpringbootApp.java
@@ -0,0 +1,17 @@
+package org.aws.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootApp {
+	
+private static final Logger log= LoggerFactory.getLogger(SpringbootApp.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApp.class,args);
+	}
+}
